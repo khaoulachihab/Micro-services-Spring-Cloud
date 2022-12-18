ccfac1feccae37464e979c6db1f551d5961636ad
khaoulachihab <chihabkhaoula1@gmail.com>
2022-12-18 15:28:16 +0100

    front end done

--- ecom-web-app/src/app/app.module.ts
+++ ecom-web-app/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import {HttpClientModule} from "@angular/common/http";
+import { CustomersComponent } from './customers/customers.component';
+import { BillsComponent } from './bills/bills.component';
+import { BillDetailsComponent } from './bill-details/bill-details.component';
 
-@NgModule({
+@NgModule({ 
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    CustomersComponent,
+    BillsComponent,
+    BillDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
